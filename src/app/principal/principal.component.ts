506d91a79ed96d5181dfb4bef442ee70097a7ec1
jonassxd <jondudys@gmail.com>
2023-10-06 16:27:53 -0300

    Criando método para listar clientes parte-2

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ClienteService } from '../service/cliente.service';
+import { Cliente } from '../model/Cliente';
 
 @Component({
   selector: 'app-principal',
@@ -10,4 +12,21 @@ export class PrincipalComponent {
     //variavel para a visibilidade dos botões
 
     btnCadastrar: boolean = true;
+
+
+  //json de clientes
+
+  clientes:Cliente[] =[];
+
+   //Construtor
+   constructor(private service:ClienteService){
+
+   }
+  //método para selecionar clientes
+
+  selecionar():void{
+    this.service.selecionar()
+    .subscribe(retorno => this.clientes = retorno);
+  }
+
 }
